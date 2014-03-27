8eca51296c2558ed75ac6179d7cea94b587601f5
Maria O.A. Sunaryo <maria.o.a.sunaryo@gdn-commerce.com>
2014-03-27 20:19:00 +0700

    bind packing presenter

--- Venice/Venice-Web/src/main/java/com/gdn/venice/client/app/VeniceClientModule.java
+++ Venice/Venice-Web/src/main/java/com/gdn/venice/client/app/VeniceClientModule.java
@@ -65,6 +65,7 @@ import com.gdn.venice.client.app.inventory.presenter.CurrencyManagementPresenter
 import com.gdn.venice.client.app.inventory.presenter.GRNCreatePresenter;
 import com.gdn.venice.client.app.inventory.presenter.GRNListPresenter;
 import com.gdn.venice.client.app.inventory.presenter.GoodIssuedNotePresenter;
+import com.gdn.venice.client.app.inventory.presenter.PackingListPresenter;
 import com.gdn.venice.client.app.inventory.presenter.PickingListPresenter;
 import com.gdn.venice.client.app.inventory.presenter.PutawayCreatePresenter;
 import com.gdn.venice.client.app.inventory.presenter.ShelfAddWithApprovalPresenter;
@@ -80,6 +81,7 @@ import com.gdn.venice.client.app.inventory.view.CurrencyManagementView;
 import com.gdn.venice.client.app.inventory.view.GRNCreateView;
 import com.gdn.venice.client.app.inventory.view.GRNListView;
 import com.gdn.venice.client.app.inventory.view.GoodIssuedNoteView;
+import com.gdn.venice.client.app.inventory.view.PackingListView;
 import com.gdn.venice.client.app.inventory.view.PickingListView;
 import com.gdn.venice.client.app.inventory.view.PutawayCreateView;
 import com.gdn.venice.client.app.inventory.view.ShelfAddWithApprovalView;
@@ -452,6 +454,11 @@ public class VeniceClientModule extends AbstractPresenterModule {
                 PutawayCreatePresenter.MyView.class,
                 PutawayCreateView.class,
                 PutawayCreatePresenter.MyProxy.class);
+        
+        bindPresenter(PackingListPresenter.class,
+                PackingListPresenter.MyView.class,
+                PackingListView.class,
+                PackingListPresenter.MyProxy.class);
 
         bindPresenter(GoodIssuedNotePresenter.class,
                 GoodIssuedNotePresenter.MyView.class,
