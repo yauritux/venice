bfbe8b11fbc88d77c35454cf5b87551f7fa4193d
Yauri Attamimi <yauritux@gmail.com>
2014-05-05 07:28:27 +0700

    add service contract for FinJournalApprovalGroup

--- Venice/Venice-Service/src/main/java/com/gdn/venice/finance/services/FinJournalApprovalGroupService.java
+++ Venice/Venice-Service/src/main/java/com/gdn/venice/finance/services/FinJournalApprovalGroupService.java
@@ -1,6 +1,7 @@
 package com.gdn.venice.finance.services;
 
 import com.gdn.venice.exception.VeniceInternalException;
+import com.gdn.venice.persistence.FinApprovalStatus;
 import com.gdn.venice.persistence.FinJournal;
 import com.gdn.venice.persistence.FinJournalApprovalGroup;
 
@@ -13,4 +14,10 @@ public interface FinJournalApprovalGroupService {
 
 	public FinJournalApprovalGroup createNew(FinJournal finJournal, String journalDescription)
 	   throws VeniceInternalException;
+	
+	public FinJournalApprovalGroup persist(FinJournalApprovalGroup finJournalApprovalGroup)
+	   throws VeniceInternalException;
+	
+	public FinJournalApprovalGroup updateApprovalStatus(FinJournalApprovalGroup finJournalApprovalGroup,
+			FinApprovalStatus finApprovalStatus) throws VeniceInternalException;
 }
