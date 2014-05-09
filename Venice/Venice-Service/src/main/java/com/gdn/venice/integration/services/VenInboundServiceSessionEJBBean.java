230f869865db92d21536a957777484fbfb1bf9eb
andri <andri.saputra@gdn-commerce.com>
2014-05-09 22:23:50 +1300

    fixed Status CR

--- Venice/Venice-Service/src/main/java/com/gdn/venice/integration/services/VenInboundServiceSessionEJBBean.java
+++ Venice/Venice-Service/src/main/java/com/gdn/venice/integration/services/VenInboundServiceSessionEJBBean.java
@@ -3204,7 +3204,7 @@ public class VenInboundServiceSessionEJBBean implements VenInboundServiceSession
                     // Synchronize the reference data then merge the status
                    venOrderItem = this.synchronizeVenOrderItemReferenceData(venOrderItem);
 
-                    List<VenOrderItem> venOrderItems = venOrder.getVenOrderItems();
+                  //  List<VenOrderItem> venOrderItems = venOrder.getVenOrderItems();
 //                    List<VenOrderItemAdjustment> venOrderItemAdjustmentNewSpreadList = new ArrayList<VenOrderItemAdjustment>();
 //                    _log.debug("Total Order Item : " + venOrderItems.size());
 
