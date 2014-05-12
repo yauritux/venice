ac68e1fcc65023717320f30afd8051e9c06401f3
Daniel HP <daniel.h.putra@gdn-commerce.com>
2014-05-12 12:26:09 +0700

    sql

---
+++ Venice/sql/scripts/venice-3.1.23.sql
@@ -0,0 +1,34 @@
+--liquibase formatted sql
+--changeset daniel:1
+CREATE TABLE frd_parameter_rule_46
+(
+  id bigint NOT NULL,
+  description character varying(40),
+  "value" integer,
+  CONSTRAINT frd_parameter_rule_46_pkey PRIMARY KEY (id)
+);
+insert into frd_parameter_rule_46 values (1,'same CC, order history<2',-10);
+insert into frd_parameter_rule_46 values (2,'same CC, order history>2',-20);
+insert into frd_parameter_rule_46 values (3,'different CC, order history<2',-5);
+insert into frd_parameter_rule_46 values (4,'different CC, order history>2',-10);
+CREATE TABLE frd_parameter_rule_47
+(
+  id bigint NOT NULL,
+  description character varying(40),
+  "value" integer,
+  CONSTRAINT frd_parameter_rule_47_pkey PRIMARY KEY (id)
+);
+insert into frd_parameter_rule_47 values (1,'same CC, order history<2',-20);
+insert into frd_parameter_rule_47 values (2,'same CC, order history>2',-30);
+insert into frd_parameter_rule_47 values (3,'different CC, order history<2',-10);
+insert into frd_parameter_rule_47 values (4,'different CC, order history>2',-15);
+--rollback drop table frd_parameter_rule_46;
+--rollback delete from frd_parameter_rule_46 where id=1;
+--rollback delete from frd_parameter_rule_46 where id=2;
+--rollback delete from frd_parameter_rule_46 where id=3;
+--rollback delete from frd_parameter_rule_46 where id=4;
+--rollback drop table frd_parameter_rule_47;
+--rollback delete from frd_parameter_rule_47 where id=1;
+--rollback delete from frd_parameter_rule_47 where id=2;
+--rollback delete from frd_parameter_rule_47 where id=3;
+--rollback delete from frd_parameter_rule_47 where id=4;
