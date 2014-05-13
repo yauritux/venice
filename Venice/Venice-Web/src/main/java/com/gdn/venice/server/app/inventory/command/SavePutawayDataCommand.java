e40e196f2e15aec96eb2d4e08789bc43d27ff06c
Roland Arya Jafar <roland.a.jafar@gdn-commerce.com>
2014-05-13 19:55:22 +0700

    move update status grn in putaway

--- Venice/Venice-Web/src/main/java/com/gdn/venice/server/app/inventory/command/SavePutawayDataCommand.java
+++ Venice/Venice-Web/src/main/java/com/gdn/venice/server/app/inventory/command/SavePutawayDataCommand.java
@@ -1,33 +1,15 @@
 package com.gdn.venice.server.app.inventory.command;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.log4j.Logger;
 
 import com.djarum.raf.utilities.Log4jLoggerFactory;
-import com.gdn.inventory.exchange.entity.AdvanceShipNoticeItem;
-import com.gdn.inventory.exchange.entity.Item;
-import com.gdn.inventory.exchange.entity.module.inbound.ConsignmentApprovalItem;
-import com.gdn.inventory.exchange.entity.module.inbound.ConsignmentFinalItem;
-import com.gdn.inventory.exchange.entity.module.inbound.GoodReceivedNoteItem;
-import com.gdn.inventory.exchange.entity.module.inbound.PurchaseOrderItem;
-import com.gdn.inventory.exchange.entity.module.inbound.PurchaseRequisitionItem;
 import com.gdn.inventory.exchange.entity.module.inbound.Putaway;
-import com.gdn.inventory.exchange.entity.module.inbound.PutawayItem;
-import com.gdn.inventory.exchange.type.ASNReferenceType;
-import com.gdn.inventory.exchange.type.PutawayType;
 import com.gdn.inventory.wrapper.ResultWrapper;
-import com.gdn.venice.client.app.DataNameTokens;
-import com.gdn.venice.server.app.inventory.service.ASNManagementService;
-import com.gdn.venice.server.app.inventory.service.GRNManagementService;
 import com.gdn.venice.server.app.inventory.service.PutawayManagementService;
 import com.gdn.venice.server.command.RafRpcCommand;
-import com.gdn.venice.server.data.RafDsRequest;
-import com.gdn.venice.server.util.Util;
-import com.gdn.venice.util.InventoryUtil;
 
 public class SavePutawayDataCommand implements RafRpcCommand {
 
